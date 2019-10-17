777e712582ba8021bb321a85bf69981582750d0d
xyyojl <m13631897256@163.com>
2019-10-17 17:05:01 +0800

    将分页功能封装成公用组件，在供应商管理模块测试好

---
+++ src/components/Pagination/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="pagination-container">
+    <el-pagination
+      :background="background"
+      :total="total"
+      :layout="layout"
+      :current-page.sync="currentPage"
+      @current-change="handleClickPage">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props:{
+    total: {
+      required: true,
+      type: Number
+    },
+    page: {
+      type: Number,
+      default: 1
+    },
+    background: {
+      type: Boolean,
+      default: true  
+    },
+    layout: {
+      type: String,
+      default: "prev, pager, next"
+    }
+  },
+  computed:{
+    currentPage:{
+      get(){
+        return this.page;
+      },
+      set(val){
+        this.$emit('update:page', val);
+      }
+    }
+  },
+  methods:{
+    handleClickPage(val){
+      this.$emit('pagination',val);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scpoed>
+.pagination-container{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+.el-pagination {
+    margin-top: 15px;
+}
+</style>
